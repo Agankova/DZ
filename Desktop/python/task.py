5f90c57b3980a98ab4ddeaa2278a8f62514e53c9
Karpenko <maria.k.visage@gmail.com>
2023-11-13 14:28:09 +0300

    task

---
+++ Desktop/python/task.py
@@ -0,0 +1,18 @@
+import argparse
+import datetime
+import logging
+logging.basicConfig(filename='logs.log', filemode='w', encoding='utf-8', level=logging.ERROR)
+
+
+def my_func(date_to_prove, value=None):
+    try:
+        return date_to_prove
+    except KeyError:
+        logging.error(f'дата недействительна')
+        return value
+ 
+
+parser = argparse.ArgumentParser()
+parser.add_argument('date', type=lambda s: datetime.datetime.strptime(s, '%d-%m-%Y'),)
+args = parser.parse_args(['78-11-2023'])
+print(args.date)
